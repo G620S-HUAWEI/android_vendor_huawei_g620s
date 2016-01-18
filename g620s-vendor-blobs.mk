a38d825ddc4de2b6e51d3415cfa95570e1ec4ab8
desalesouche <desalesouche@gmail.com>
2016-01-18 05:05:51 +0100

    sensors: Add gyroscope
    Change-Id: Ifabcba6442fad5576180c01fabf1f26323d4f8f6

--- g620s-vendor-blobs.mk
+++ g620s-vendor-blobs.mk
@@ -589,6 +589,7 @@ PRODUCT_COPY_FILES += \
     $(PATH_FILES)/vendor/lib/libbtnv.so:system/vendor/lib/libbtnv.so \
     $(PATH_FILES)/vendor/lib/libc2d30-a3xx.so:system/vendor/lib/libc2d30-a3xx.so \
     $(PATH_FILES)/vendor/lib/libC2D2.so:system/vendor/lib/libC2D2.so \
+    $(PATH_FILES)/vendor/lib/libcalmodule_akm.so:system/vendor/lib/libcalmodule_akm.so \
     $(PATH_FILES)/vendor/lib/libCB.so:system/vendor/lib/libCB.so \
     $(PATH_FILES)/vendor/lib/libcne.so:system/vendor/lib/libcne.so \
     $(PATH_FILES)/vendor/lib/libcneapiclient.so:system/vendor/lib/libcneapiclient.so \
